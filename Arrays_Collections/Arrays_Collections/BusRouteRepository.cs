472507dad23f5aee4d4a93b6c656bf659202dbee
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-05 10:17:33 +0200

    Demo inicial modulo 4

--- Arrays_Collections/Arrays_Collections/BusRouteRepository.cs
+++ Arrays_Collections/Arrays_Collections/BusRouteRepository.cs
@@ -6,18 +6,20 @@ using System.Threading.Tasks;
 
 namespace Pluralsight.ArraysCollections.Demos
 {
-	public class BusRouteRepository
+	class BusRouteRepository
 	{
 		public static BusRoute[] InitializeRoutes()
 		{
 			return new BusRoute[] {
-				new BusRoute(40, new string[] {
+				new BusRoute(40, new string[]{
 					"Morecambe", "Lancaster", "Garstang", "Preston" }),
-				new BusRoute(42, new string[] { "Lancaster", "Garstang", "Blackpool" }),
-				new BusRoute(100, new string[] { "University", "Lancaster", "Morecambe" }),
-				new BusRoute(555, new string[] {
-					"Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" })
+				new BusRoute(42, new string[]{"Lancaster", "Garstang", "Blackpool" }),
+				new BusRoute(5, new string[]{"Overton", "Morecambe", "Carnforth" }),
+				new BusRoute(555, new string[]{
+					"Lancaster", "Carnforth", "Kendal", "Windermere", "Keswick" }),
+				new BusRoute(1_000, new string[] { "Test origin", "Test destination" }),
 			};
 		}
 	}
+
 }
--- Arrays_Collections/Arrays_Collections/Program.cs
+++ Arrays_Collections/Arrays_Collections/Program.cs
@@ -9,21 +9,16 @@ namespace Pluralsight.ArraysCollections.Demos
 		{
 			BusRoute[] allRoutes = BusRouteRepository.InitializeRoutes();
 
-			Console.WriteLine("Where do you want to go to?");
-			string location = Console.ReadLine();
+			Console.WriteLine($"Before: There are {allRoutes.Length} routes:");
+			foreach (BusRoute route in allRoutes)
+				Console.WriteLine($"Route: {route}");
 
-			BusRoute[] routes = FindBusesTo(allRoutes, location);
+			// this isn't really a good way of removing an item - but it happens to work here
+			Array.Resize(ref allRoutes, 4);
 
-			if (routes.Length > 0)
-				foreach (BusRoute route in routes)
-					Console.WriteLine($"You can use route {route}");
-			else
-				Console.WriteLine($"No routes go to {location}");
-		}
-
-		public static BusRoute[] FindBusesTo(BusRoute[] routes, string location)
-		{
-			return Array.FindAll(routes, route => route.Serves(location));
+			Console.WriteLine($"\r\nAfter: There are {allRoutes.Length} routes:");
+			foreach (BusRoute route in allRoutes)
+				Console.WriteLine($"Route: {route}");
 		}
 	}
 }
